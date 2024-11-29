cc6df83847ab989581c2de76a0d4c0014f3b211c
Leonid Prokoshkin <prokoshkin@ddplanet.ru>
2024-11-29 17:34:26 +0300

    add burger and navbar default styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { useEffect } from "react"
 import { Outlet, useNavigate } from "react-router-dom"
 
-import { AppShell, Flex, Group } from "@mantine/core"
+import { AppShell, Button, Flex } from "@mantine/core"
+import { useDisclosure } from "@mantine/hooks"
 
 import { Header } from "./components/Header.tsx"
 import { PAGES } from "./pages/utils/routes.constants.ts"
 
 function App() {
   const navigate = useNavigate()
+  const [opened, { toggle }] = useDisclosure(false)
 
   useEffect(() => {
     navigate(PAGES.MAIN.INDEX)
@@ -15,18 +17,26 @@ function App() {
 
   return (
     <AppShell
-      h="100%"
-      display="flex"
-      withBorder={false}
-      layout="alt"
       header={{ height: 72 }}
+      navbar={{
+        width: 300,
+        breakpoint: "sm",
+        collapsed: { desktop: true, mobile: !opened },
+      }}
       padding="md"
     >
       <AppShell.Header bg="grey">
-        <Group h="100%" w="100%">
-          <Header />
-        </Group>
+        <Header opened={opened} toggle={toggle} />
       </AppShell.Header>
+
+      <AppShell.Navbar py="md" px={4}>
+        <Button variant="default">Главная</Button>
+        <Button variant="default">Услуги</Button>
+        <Button variant="default">CКаталог б/у Резины</Button>
+        <Button variant="default">Акции и скидки</Button>
+        <Button variant="default">Контакты</Button>
+      </AppShell.Navbar>
+
       <AppShell.Main pb={0} pr={0} w="100%">
         <Flex direction="column" h="100%" w="100%">
           <Outlet />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,25 @@
-import { Flex } from "@mantine/core"
+import { FC } from "react"
 
-export const Header = () => {
+import { Burger, Button, Group } from "@mantine/core"
+
+interface HeaderProps {
+  opened: boolean
+  toggle(): void
+}
+export const Header: FC<HeaderProps> = ({ opened, toggle }) => {
   return (
-    <Flex justify="space-between">
-      <Flex>Главная</Flex>
-      <Flex>Каталог</Flex>
-    </Flex>
+    <Group h="100%" px="md">
+      <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+      <Group justify="space-between" style={{ flex: 1 }}>
+        <div>LOGO</div>
+        <Group ml="xl" gap="md" visibleFrom="sm">
+          <Button variant="default">Главная</Button>
+          <Button variant="default">Услуги</Button>
+          <Button variant="default">CКаталог б/у Резины</Button>
+          <Button variant="default">Акции и скидки</Button>
+          <Button variant="default">Контакты</Button>
+        </Group>
+      </Group>
+    </Group>
   )
 }
